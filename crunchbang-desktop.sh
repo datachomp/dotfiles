f4ce83bed8c9cc2141a0ee73cf4c02d7bb967f94
Rob Sullivan <rob@datachomp.com>
2013-07-08 21:54:34 -0500

    fixing crunchy dependencies

--- crunchbang-desktop.sh
+++ crunchbang-desktop.sh
@@ -3,22 +3,23 @@ sudo apt-get install -y python-software-properties software-properties-common cu
 sudo apt-get update
 
 #db
-sudo echo "deb http://apt.postgresql.org/pub/repos/apt/ wheezy-pgdg main" > /etc/apt/sources.list.d/pgdg.list
+echo "deb http://apt.postgresql.org/pub/repos/apt/ wheezy-pgdg main" > ~/pgdg.list
+sudo mv ~/pgdg.list /etc/apt/sources.list.d/pgdg.list
 sudo wget --quiet -O - http://apt.postgresql.org/pub/repos/apt/ACCC4CF8.asc | sudo apt-key add -
-sudo apt-get update
-sudo apt-get install -y postgresql-9.2 postgresql-9.2-common pgadmin3
 
 #node
-sudo echo "deb http://ppa.launchpad.net/chris-lea/node.js/ubuntu lucid main" > /etc/apt/sources.list.d/chris-lea-node_js-wheezy.list
+echo "deb http://ppa.launchpad.net/chris-lea/node.js/ubuntu lucid main" > ~/chris-lea-node_js-wheezy.list
+sudo mv ~/chris-lea-node_js-wheezy.list /etc/apt/sources.list.d/chris-lea-node_js-wheezy.list
 sudo apt-get update
 
+#sublimetext
+sudo echo "webupd8team-sublime-text-2-wheezy.list" > /etc/apt/sources.list.d/webupd8team-sublime-text-2-wheezy.list
+
+sudo apt-get install -y postgresql-9.2 pgadmin3 libpq-dev postgresql-client
 
 sudo apt-get install -y nodejs
-sudo apt-get install -y libpq-dev postgresql-client 
 sudo apt-get install -y p7zip-full
 
-sudo echo "webupd8team-sublime-text-2-wheezy.list" > /etc/apt/sources.list.d/webupd8team-sublime-text-2-wheezy.list
-sudo apt-get update
 sudo apt-get install -y sublime-text
 
 
