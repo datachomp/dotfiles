000be15e3c2cfa1b8a061aefc1d5203be2cadf54
rob sullivan <rob@datachomp.com>
2013-07-23 22:47:35 -0500

    cleaning up the ubuntu installer

--- ubuntu/ubuntu_desktop_setup.sh
+++ ubuntu/ubuntu_desktop_setup.sh
@@ -2,56 +2,55 @@
 echo "Get the basics"
 # apt-get -y update   # Is this overkill at this step?
 # This lets us just add ppas
-apt-get -y install python-software-properties
-apt-get -y install software-properties-common
+sudo apt-get -y install python-software-properties software-properties-common
 
 #hurry and load apt-fast
-add-apt-repository -y ppa:apt-fast/stable
-apt-get update
-apt-get install -y apt-fast 
+sudo add-apt-repository -y ppa:apt-fast/stable
+sudo apt-get update
+sudo apt-get install -y apt-fast 
 
 echo "Add PPAs"
-add-apt-repository -y ppa:texworks
-add-apt-repository -y ppa:ubuntugis/ubuntugis-unstable
+sudo add-apt-repository -y ppa:texworks
+sudo add-apt-repository -y ppa:ubuntugis/ubuntugis-unstable
 
-add-apt-repository -y ppa:tualatrix/ppa
-add-apt-repository -y ppa:nginx/stable 
-add-apt-repository -y ppa:pitti/postgresql 
-add-apt-repository -y ppa:chris-lea/node.js
-add-apt-repository -y ppa:chris-lea/redis-server
-add-apt-repository -y ppa:webupd8team/sublime-text-2
-add-apt-repository -y ppa:nvbn-rm/ppa
-add-apt-repository -y ppa:conscioususer/polly-daily
-add-apt-repository -y ppa:yorba/ppa
-add-apt-repository -y ppa:indicator-multiload/stable-daily
-add-apt-repository -y ppa:cooperjona/lightread
+sudo add-apt-repository -y ppa:tualatrix/ppa
+sudo add-apt-repository -y ppa:nginx/stable 
+sudo add-apt-repository -y ppa:pitti/postgresql 
+sudo add-apt-repository -y ppa:chris-lea/node.js
+sudo add-apt-repository -y ppa:chris-lea/redis-server
+sudo add-apt-repository -y ppa:webupd8team/sublime-text-2
+sudo add-apt-repository -y ppa:nvbn-rm/ppa
+sudo add-apt-repository -y ppa:conscioususer/polly-daily
+sudo add-apt-repository -y ppa:yorba/ppa
+sudo add-apt-repository -y ppa:indicator-multiload/stable-daily
+sudo add-apt-repository -y ppa:cooperjona/lightread
 
 #pgadmin 1.16
-apt-add-repository -y ppa:voronov84/andreyv
+sudo apt-add-repository -y ppa:voronov84/andreyv
 
 echo "Updating Repos"
-apt-get update
+sudo apt-get update
 
 echo "Installing packages"
 #system tweaks
-apt-fast install -y preload ubuntu-tweak indicator-multiload conky gdebi
+sudo apt-fast install -y preload ubuntu-tweak indicator-multiload conky gdebi
 
 #Dev Tools
-apt-fast install -y nginx postgresql-9.2 postgresql-contrib libpq-dev pgadmin3 nodejs sublime-text
-apt-fast install -y redis-server
+sudo apt-fast install -y nginx postgresql-9.2 postgresql-contrib libpq-dev pgadmin3 nodejs sublime-text
+sudo apt-fast install -y redis-server
 #apt-fast install -y mono-complete monodevelop meld
-apt-fast install -y meld
+sudo apt-fast install -y meld
 
 #media
-apt-fast install -y vlc vlc-plugin-pulse mozilla-plugin-vlc
-apt-fast install -y openshot #video editor
+sudo apt-fast install -y vlc vlc-plugin-pulse mozilla-plugin-vlc
+sudo apt-fast install -y openshot #video editor
 
 #misc
-apt-fast install -y filezilla everpad polly
-apt-fast install -y shotwell curl nautilus-open-terminal
-apt-fast install -y lightread pidgin
-apt-fast install -y chromium-browser
-apf-fast install -y p7zip-rar p7zip-full
+sudo apt-fast install -y filezilla everpad polly
+sudo apt-fast install -y shotwell curl nautilus-open-terminal
+sudo apt-fast install -y lightread pidgin
+sudo apt-fast install -y chromium-browser
+sudo apt-fast install -y p7zip-rar p7zip-full
 
 #testing
 #sudo add-apt-repository ppa:atareao/atareao
@@ -59,34 +58,37 @@ apf-fast install -y p7zip-rar p7zip-full
 
 
 echo "make sure mysql is toast"
-update-rc.d mysql remove
-update-rc.d apache2 remove
+sudo update-rc.d mysql remove
+sudo update-rc.d apache2 remove
 
 echo "rvm time"
-apt-fast install -y git-core curl build-essential zlib1g-dev libreadline-dev libssl-dev libxml2-dev git-gui git-doc
+sudo apt-fast install -y git-core curl build-essential zlib1g-dev libreadline-dev libssl-dev libxml2-dev git-gui git-doc
 #bash -s stable < <(curl -s https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer)
-curl -L https://get.rvm.io | bash -s stable --ruby
-apt-fast install -y openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev automake libtool bison subversion
+sudo apt-fast install -y openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev automake libtool bison subversion
 
+curl -L https://get.rvm.io | bash -s stable --ruby
 echo '[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"' >> ~/.bashrc
 echo 'export PATH=$HOME/.rvm/bin:$PATH' >> ~/.bashrc
 
 #this lens is just way too slow
-apt-get remove unity-lens-shopping
+sudo apt-get remove unity-lens-shopping
 
 #show all startup apps in startup applications
 sudo sed -i 's/NoDisplay=true/NoDisplay=false/g' /etc/xdg/autostart/*.desktop
 
 #normal scrolling
-gsettings set com.canonical.desktop.interface scrollbar-mode normal
+sudo gsettings set com.canonical.desktop.interface scrollbar-mode normal
 
 # move the x - []  icons to the right side
-gconftool-2 --set /apps/metacity/general/button_layout --type string menu:minimize,maximize,close
+#sudo gconftool-2 --set /apps/metacity/general/button_layout --type string menu:minimize,maximize,close
 
+#if you need some  virtualization love
+#https://help.ubuntu.com/community/VMware/Tools
+#sudo apt-get install -y open-vm-tools
 
 
 echo "clean up"
-apt-get autoclean
+sudo apt-get autoclean
 
 echo "horray!"
 exit 0
