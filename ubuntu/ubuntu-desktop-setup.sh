e3abf767323cede3df7ea1bea245e41f46faff26
rob sullivan <rob@datachomp.com>
2016-03-27 11:01:44 -0500

    update rvm installer

--- ubuntu/ubuntu-desktop-setup.sh
+++ ubuntu/ubuntu-desktop-setup.sh
@@ -41,9 +41,11 @@ sudo apt-get install -y neovim sublime-text-installer
 
 # languages
 sudo apt-get install -y golang-go ubuntu-sdk
+
 echo "install ruby"
-curl -L https://get.rvm.io | bash -s stable --autolibs=3
-source ~/.bash_profile
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+\curl -sSL https://get.rvm.io | bash -s stable
+source ~/.rvm/scripts/rvm
 rvm install 2.3.0
 rvm --default use 2.3.0
 
