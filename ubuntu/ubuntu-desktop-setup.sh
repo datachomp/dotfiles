5b4ed199b6e64b3743e6f0f2d6033db38d165786
rob sullivan <rob@datachomp.com>
2016-03-27 00:32:08 -0500

    add handbreak

--- ubuntu/ubuntu-desktop-setup.sh
+++ ubuntu/ubuntu-desktop-setup.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 echo "Get the basics"
 
-mkdir ~/projects
+mkdir -p ~/projects
 
-sudo apt-add-repository ppa:me-davidsansome/clementine
-sudo add-apt-repository ppa:yorba/ppa
-sudo add-apt-repository ppa:neovim-ppa/unstable
-sudo add-apt-repository ppa:webupd8team/sublime-text-3
-sudo add-apt-repository ppa:ys/emojione-picker
-
+sudo apt-add-repository -y ppa:me-davidsansome/clementine
+sudo add-apt-repository -y ppa:yorba/ppa
+sudo add-apt-repository -y ppa:neovim-ppa/unstable
+sudo add-apt-repository -y ppa:webupd8team/sublime-text-3
+sudo add-apt-repository -y ppa:ys/emojione-picker
+sudo add-apt-repository -y ppa:stebbins/handbrake-releases
 
 # postgres time!
 sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
 sudo wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
 sudo apt-get update
-sudo apt-get install -y postgresql-9.5 postgresql-contrib-9.5 postgresql-client-9.5 pgbouncer
-
+sudo apt-get install -y postgresql-9.5 postgresql-contrib-9.5 postgresql-client-9.5 pgbouncer pgadmin3 libpq-dev
 
 # utilities
 sudo apt-get install -y htop openssh-server
@@ -25,12 +24,13 @@ sudo apt-get install -y git curl
 sudo apt-get install -y ubuntu-wallpapers* ntp ntpdate
 
 # codecs
-sudo apt-get install -y  libxine1-ffmpeg mencoder flac faac faad sox ffmpeg2theora libmpeg2-4 uudeview libmpeg3-1 mpeg3-utils mpegdemux liba52-dev mpeg2dec vorbis-tools id3v2 mpg321 mpg123 libflac++6 totem-mozilla icedax lame libmad0 libjpeg-progs libdvdcss2 libdvdread4 libdvdnav4 libswscale-extra-2 ubuntu-restricted-extras
+sudo apt-get install -y handbrake-gtk handbrake-cli
+sudo apt-get install -y libxine1-ffmpeg mencoder flac faac faad sox ffmpeg2theora libmpeg2-4 uudeview libmpeg3-1 mpeg3-utils mpegdemux liba52-dev mpeg2dec vorbis-tools id3v2 mpg321 mpg123 libflac++6 totem-mozilla icedax lame libmad0 libjpeg-progs libdvdcss2 libdvdread4 libdvdnav4 libswscale-extra-2 ubuntu-restricted-extras
 
 
 # social
 echo "install corebird from ubuntu store"
-sudo apt-get install emojione-picker
+sudo apt-get install -y emojione-picker
 
 # email
 sudo apt-get install -y geary
