42971ea21e341529928f51b04248eba94f6f7eca
Rob Sullivan <rob@datachomp.com>
2016-06-29 12:12:46 -0500

    fix home-brew installer

--- homebrew_install.sh
+++ homebrew_install.sh
@@ -2,7 +2,7 @@
 # Install if we don't have it
 if test ! $(which brew); then
   echo "Installing homebrew..."
-  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
 # Update homebrew recipes
