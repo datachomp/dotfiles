ca90e524d3e7c6224ae6c060d0b54a5ff8c058ef
Rob Sullivan <rob@datachomp.com>
2016-07-02 16:22:07 -0500

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -3,7 +3,6 @@
 BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # git
-ln -s ${BASEDIR}/.git ~/
 ln -s ${BASEDIR}/.gitconfig ~/
 ln -s ${BASEDIR}/.gitignore ~/
 
@@ -31,3 +30,26 @@ ln -s ${BASEDIR}/.vimrc ~/
 
 #hostfile
 ln -s ${BASEDIR}/host_file_update.sh ~/
+
+
+
+
+
+# easier install?
+curl -o .curlrc https://raw.githubusercontent.com/datachomp/dotfiles/master/.curlrc
+curl -o .gemrc https://raw.githubusercontent.com/datachomp/dotfiles/master/.gemrc
+curl -o .gitconfig https://raw.githubusercontent.com/datachomp/dotfiles/master/.gitconfig
+curl -o .gitignore https://raw.githubusercontent.com/datachomp/dotfiles/master/.gitignore
+curl -o .irbrc https://raw.githubusercontent.com/datachomp/dotfiles/master/.irbrc
+curl -o .psqlrc https://raw.githubusercontent.com/datachomp/dotfiles/master/.psqlrc
+curl -o .railsrc https://raw.githubusercontent.com/datachomp/dotfiles/master/.railsrc
+curl -o .ruby-settings https://raw.githubusercontent.com/datachomp/dotfiles/master/.ruby-settings
+
+curl -o .wgetrc https://raw.githubusercontent.com/datachomp/dotfiles/master/.wgetrc
+curl -o .curlrc https://raw.githubusercontent.com/datachomp/dotfiles/master/.curlrc
+curl -o .aliases https://raw.githubusercontent.com/datachomp/dotfiles/master/.aliases
+
+curl -o host_file_update.sh https://raw.githubusercontent.com/datachomp/dotfiles/master/host_file_update.sh
+
+
+
