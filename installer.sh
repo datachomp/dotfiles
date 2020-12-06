7210c4c86a326c20bc8e51c39978d879b98adb0b
rob sullivan <rob@datachomp.com>
2020-12-05 18:01:30 -0600

    better installer options

--- installer.sh
+++ installer.sh
@@ -5,17 +5,45 @@ BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 OS=$1
 HOSTNAME=$2
 
-
-echo "$1"
-echo "$2"
-
 if [ $OS == "macos" ]
 then
-   echo "You've got max"
-
+	# git
+	ln -s ${BASEDIR}/.gitconfig ~/
+	ln -s ${BASEDIR}/.gitignore ~/
+	# ruby
+	ln -s ${BASEDIR}/.gemrc ~/
+	ln -s ${BASEDIR}/.irbrc ~/
+	ls -s ${BASEDIR}/.railsrc ~/
+	ls -s ${BASEDIR}/.ruby-settings ~/
+	#web pullers
+	ln -s ${BASEDIR}/.curlrc ~/
+	ln -s ${BASEDIR}/.wgetrc ~/
+	#postgres
+	ln -s ${BASEDIR}/.psqlrc ~/
+	#editors
+	ln -s ${BASEDIR}/.sublime_settings ~/
+	ln -s ${BASEDIR}/.tmux.conf ~/
+	ln -s ${BASEDIR}/.vimrc ~/
 elif [ $OS == "ubuntu" ]
 then
-	echo "You've got max"	
+	echo "You've got linners"
+	# git
+	ln -s ${BASEDIR}.gitconfig ~/
+	ln -s ${BASEDIR}.gitignore ~/
+	# ruby
+	ln -s ${BASEDIR}/.gemrc ~/
+	ln -s ${BASEDIR}/.irbrc ~/
+	ls -s ${BASEDIR}/.railsrc ~/
+	ls -s ${BASEDIR}/.ruby-settings ~/
+	#web pullers
+	ln -s ${BASEDIR}/.curlrc ~/
+	ln -s ${BASEDIR}/.wgetrc ~/
+	#postgres
+	ln -s ${BASEDIR}/.psqlrc ~/
+	#editors
+	ln -s ${BASEDIR}/.sublime_settings ~/
+	ln -s ${BASEDIR}/.tmux.conf ~/
+	ln -s ${BASEDIR}/.vimrc ~/
 else
    echo "You need to select a better option"
 fi
